d2353a9d38606ecc5bb598935698a75898716e97
William <w.asiata@gmail.com>
2017-12-19 13:08:18 +1300

    created taumahi setup package

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='taumahi',
       version='1.0',
-      py_modules=['taumahi'],
+      packages=['taumahi'],
+      package_dir={'taumahi': 'taumahi'},
+      package_data={'taumahi': ['*.txt']
       )
