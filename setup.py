78f4cdd4fb74e12c12059f0e68af17e63f84f488
Max <mbun189@aucklanduni.ac.nz>
2018-01-23 13:42:56 +1300

    Updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import sys
 
 setup(name='taumahi',
-      version='1.1',
+      version='1.2',
       py_modules=['taumahi'],
       data_files=[('lib/python{}/dist-packages/taumahi_tūtira'.format(sys.version[0:3]), ['taumahi_tūtira/kupu_kino_kūare_tohutō.txt', 'taumahi_tūtira/kupu_kino.txt',
                                                                                           'taumahi_tūtira/kupu_rangirua_kūare_tohutō.txt', 'taumahi_tūtira/kupu_rangirua.txt'])]
