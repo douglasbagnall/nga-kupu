fa0588b5027b2e9172c6db082f1bd32d65c7ace3
William <w.asiata@gmail.com>
2018-02-01 17:32:07 +1300

    debugged regex

--- taumahi.py
+++ taumahi.py
@@ -265,10 +265,10 @@ sentence_end = ['([.!?:—"]+[\)\]]*|,\s*")', '[{}]+'.format(apostrophes)]
 new_paragraph = re.compile(
     '({}+|-+){}\n'.format(sentence_end[0], sentence_end[1]))
 paragraph_pattern = re.compile(
-    '(?<=([.!?]|[:-—]))[-—.!? ‘’\'"•]*\n["\']*(?=[A-Z])')
+    '(?<=([.!?]|[\-—:]))[\-—.!? ‘’\'"•]*\n["\']*(?=[A-Z])')
 
 # Regex to detect the end of a sentence
-new_sentence = re.compile('{}{}'.format(sentence_end[0], sentence_end[1]))
+new_sentence = re.compile('{}{}|"—'.format(sentence_end[0], sentence_end[1]))
 
 
 def get_paragraph(txt):
